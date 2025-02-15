f2aedec4ab9454f2da6b0d4dd344da9f226cec94
carloszamorano10 <ca.zamorano@hotmail.com>
2025-02-14 23:16:04 -0300

    avances segundo dia

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,38 @@ const input = document.getElementById("inputNumeros");
 const selectMoneda = document.getElementById("selectorMoneda");
 const btn = document.getElementById("btn");
 const resultado = document.getElementById("resultado");
+const cargando = document.getElementById("load");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", async () => {
   const valorIngresado = parseFloat(input.value);
-  console.log(valorIngresado);
+  const monedaSel = selectMoneda.value;
+
+  const valorMoneda = await traeMoneda(monedaSel);
+  if (valorMoneda) {setTimeout(()=>{
+    const conv = valorIngresado / valorMoneda;
+    resultado.textContent = `Resultado: ${conv.toFixed(2)} ${monedaSel}` 
+  },1000)
+  }
 });
+
+async function traeMoneda(moneda) {
+  try {
+    cargando.style.display = "block";
+    const res = await fetch("https://mindicador.cl/api");
+    const data = await res.json();
+    if (data[moneda]) {
+      return data[moneda].valor;
+    } else {
+      console.error(`La moneda "${moneda}" no fue encontrada.`);
+      alert("Moneda no válida.");
+      return null;
+    }
+  } catch (error) {
+    alert("Hubo un error en la carga...");
+    return null;
+  } finally {
+    setTimeout(() => {
+      cargando.style.display = "none";
+    }, 1000);
+  }
+}
